c6fca0ee9e5277472dbc8d3b9ba4d4516f73114f
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-05 12:13:39 +0530

    Use Case 2 - Calculating Expr (a+b*c)

--- SortArithmeticComputation.sh
+++ SortArithmeticComputation.sh
@@ -4,6 +4,20 @@ echo "Welcome to Sorting Arithmetic Computation Problem"
 
 #Use Case 1 (Taking User Inputs)
 
+#read -p "Enter the value of a: " a
+#read -p "Enter the value of b: " b
+#read -p "Enter the value of c: " c
+
+
+
+
+#Use Case 2 (Compute ( a + b * c ))
+
 read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
+
+#Compute ( a + b * c )
+
+expr1=`awk "BEGIN {print $a + ( $b * $c)}"`
+echo "a+b*c="$expr1
