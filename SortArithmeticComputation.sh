75ca1fab5b9f4288f7797b4acf1c7bef01a61669
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-05 12:07:49 +0530

    Use Case 1 - Taking User Inputs

--- SortArithmeticComputation.sh
+++ SortArithmeticComputation.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -x
 
-
 echo "Welcome to Sorting Arithmetic Computation Problem"
+
+#Use Case 1 (Taking User Inputs)
+
+read -p "Enter the value of a: " a
+read -p "Enter the value of b: " b
+read -p "Enter the value of c: " c
