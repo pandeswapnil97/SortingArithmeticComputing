5dbaa5a089df13d747f78ff5158afe07a422ca88
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-05 12:40:31 +0530

    Use Case 7 - Reading Values from Dictionary into Array

--- SortArithmeticComputation.sh
+++ SortArithmeticComputation.sh
@@ -74,13 +74,67 @@ echo "Welcome to Sorting Arithmetic Computation Problem"
 
 #Use Case 6 (Storing the results into dictionary)
 
+#declare -A expr_result
+#
+#read -p "Enter the value of a: " a
+#read -p "Enter the value of b: " b
+#read -p "Enter the value of c: " c
+#
+#
+#
+##Compute ( a + b * c )
+#
+#expr1=`awk "BEGIN {print $a + ( $b * $c)}"`
+#echo "a+b*c="$expr1
+#
+#
+##Compute ( a * b + c )
+#
+#expr2=`awk "BEGIN {print ($a * $b) + $c}"`
+#echo "a*b+c="$expr2
+#
+#
+##Compute ( c + a / b )
+#
+#expr3=`awk "BEGIN {print $c + ($a / $b)}"`
+#echo "c+a/b="$expr3
+#
+#
+##Compute ( a % b + c )
+#
+#expr4=`awk "BEGIN {print ($a % $b) + $c}"`
+#echo "a%b+c="$expr4
+#
+#echo "        "
+#
+#expr_result[a+b*c]=$expr1
+#expr_result[a*b+c]=$expr2
+#expr_result[c+a/b]=$expr3
+#expr_result[a%b+c]=$expr4
+#
+#echo "Computed Dictionary:"
+#
+#for i in "${!expr_result[@]}"
+#do
+#	echo "$i:${expr_result[$i]}"
+#done
+
+
+
+
+#use Case 7 (Reading values from dictionary into array)
+
 declare -A expr_result
+
+values_array_counter=0
+
 
 read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
 
 
+echo "          "
 
 #Compute ( a + b * c )
 
@@ -116,5 +170,16 @@ echo "Computed Dictionary:"
 
 for i in "${!expr_result[@]}"
 do
-	echo "$i:${expr_result[$i]}"
+       echo "$i:${expr_result[$i]}"
 done
+
+for i in "${expr_result[@]}"
+do
+	values_array[((values_array_counter++))]=$i
+done
+
+echo "         "
+
+echo "Array of values in dictionary: "${values_array[@]}
+
+echo "         "
